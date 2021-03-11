d4c478227224913ebde35814f2a6d37ccaa4ff9f
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-03-10 22:49:10 -0300

    criando biblioteca simples

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+type EventProps = Event & {
+  detail?: {
+    userName?: string
+  }
+}
+
 function App() {
+
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+
+    document.addEventListener('hub-infos', (e: EventProps) => {
+      setName(e?.detail?.userName || '')
+    }, false)
+
+  },[])
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {name && (
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Olá, {name}
         </p>
+
+        )}
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#!"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bem vindo ao Micro-Frontend
         </a>
       </header>
     </div>
